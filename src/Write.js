eec012b9da50497e130cad275898e0ddcda63864
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-04-22 21:28:03 +0900

    map함수

--- src/Write.js
+++ src/Write.js
@@ -4,25 +4,56 @@ import { useHistory } from "react-router-dom";
 function Write() {
     let history = useHistory();
 
-    let [je, setJe] = useState('');
-    let [mock, setMock] = useState('');
+    // let [list, setList] = useState(['']);
 
-    let [nae, setNae] = useState('');
-    let [young, setYoung] = useState('');
+    let [je, setJe] = useState(['']);
+    let [mock, setMock] = useState(['첫번째 제목']);
+
+    let [nae, setNae] = useState(['']);
+    let [young, setYoung] = useState(['첫번째 글']);
+
+    function addJemock() {
+        let newMock = [...mock];
+        newMock.push(je);
+        setMock(newMock);
+    }
+
+    function addNaeyoung() {
+        let newYoung = [...young];
+        newYoung.push(nae);
+        setYoung(newYoung);
+    }
 
     return(
     <div>
     <p/>제목 <input onChange={(e)=>{setJe(e.target.value);}}/> 
     <p/>내용 <input onChange={(e)=>{setNae(e.target.value);}}/> 
         
-        <button onClick={(e)=>{setMock(je); setYoung(nae);}}>완료</button>
-        <hr/>
-        <p/> 제목 : {mock}
-        <p/> 내용 : {young}
-
         <p/><button onClick={()=> {
             history.goBack();
         }}>뒤로가기</button>
+        {/* <button onClick={(e)=>{setMock(je); setYoung(nae);}}>완료</button> */}
+        <button onClick={(e)=>{{addJemock(); addNaeyoung();}}}>완료</button>
+        <hr/>
+        {/* <hr/>
+        <p/> 제목 : {mock}
+        <p/> 내용 : {young} */}
+
+        {/* array.map(currentValue[,index[,array]])
+      currentValue : 처리할 현재 요소
+      index : 처리할 현재 요소의 인덱스(0부터 시작)
+      map()으로 호출한 배열 */}
+
+        {mock.map(function (mock, i) {
+        return (
+          <div className='mock' key={i}>
+            <h3>제목 : {mock}</h3>
+            <h3>내용 : {young}</h3>
+            <hr/>
+          </div>
+        );
+      })
+      }
 
         {/* 저장 만들기 */}
     </div>
